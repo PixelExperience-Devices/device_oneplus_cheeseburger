b05b8dae011af9a56cf718667e02ef74333cad31
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-05-30 21:13:03 -0400

    cheeseburger: Add back touch HAL
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,3 +32,5 @@ TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5,oneplus5
 
 # inherit from the proprietary version
 -include vendor/oneplus/cheeseburger/BoardConfigVendor.mk
+
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
