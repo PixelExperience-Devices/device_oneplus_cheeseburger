171cfef1bbbe096850f2649de5ce57a5629a52e7
LuK1337 <priv.luk@gmail.com>
2020-06-23 15:05:34 +0200

    cheeseburger: Unset PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE

--- device.mk
+++ device.mk
@@ -314,9 +314,6 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# Properties
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
