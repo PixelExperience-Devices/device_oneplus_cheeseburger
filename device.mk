8c460a3a6b9a5a64f3352c73de5e894cd0bd546f
xLexip <x@lexip.dev>
2022-01-27 17:02:08 +0100

    cheeseburger: Drop overlay-lineage

--- device.mk
+++ device.mk
@@ -25,9 +25,7 @@
 $(call inherit-product, vendor/oneplus/cheeseburger/cheeseburger-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
