e353b269c25162a8c225ba1837dcc7bbbe7c51db
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:16:08 -0500

    cheeseburger: Move super_dummy build to Android.mk
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- Android.mk
+++ Android.mk
@@ -10,6 +10,19 @@ ifeq ($(TARGET_DEVICE),cheeseburger)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+LPFLASH := $(HOST_OUT_EXECUTABLES)/lpflash$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_SUPERIMAGE_DUMMY_TARGET := $(PRODUCT_OUT)/super_dummy.img
+
+$(INSTALLED_SUPERIMAGE_DUMMY_TARGET): $(PRODUCT_OUT)/super_empty.img $(LPFLASH)
+	$(call pretty,"Target dummy super image: $@")
+	$(hide) touch $@
+	$(hide) $(LPFLASH) $@ $(PRODUCT_OUT)/super_empty.img
+
+.PHONY: super_dummyimage
+super_dummyimage: $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
 include $(CLEAR_VARS)
 
 EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/
