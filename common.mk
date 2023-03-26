1f3cec99d8b16fd3d2fd3e3744854e686e848f49
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-03-26 17:26:22 +0530

    msm8998-common: Drop LiveDisplay, Touch and Trust
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- common.mk
+++ common.mk
@@ -229,10 +229,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.oneplus_msm8998
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.oneplus_msm8998
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -380,10 +376,6 @@ PRODUCT_PACKAGES += \
 # Tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
