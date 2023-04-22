4f25ac9bfd60c460e4ea69a60acdd5dc7a904385
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-04-23 02:42:40 +0530

    cheeseburger: Update DRM from phoenix_sprout
     * phoenix_lao_com-user 11 RKQ1.201123.002 21274101794d9 release-keys
    
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- device.mk
+++ device.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
